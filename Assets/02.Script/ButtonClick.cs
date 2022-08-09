5761a505f87e5fae5be005e52bd58bb22b0c5483
jongsungs <78831578+jongsungs@users.noreply.github.com>
2022-08-09 17:19:23 +0900

    조이스틱 추가 및 로비 씬 추가

---
+++ Assets/02.Script/ButtonClick.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonClick : MonoBehaviour,IPointerDownHandler,IPointerUpHandler
+{
+    RectTransform _rectTrans;
+    bool _isButtonDown = false;
+    private void Awake()
+    {
+        _rectTrans = this.GetComponent<RectTransform>();
+    }
+
+
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        _isButtonDown = true;
+
+        
+
+        Debug.Log("눌렀다");
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        _isButtonDown = false;
+        
+        
+    }
+
+    private void Update()
+    {
+        if(_isButtonDown)
+        {
+            if (_rectTrans.localScale.x >= 0.8f && _rectTrans.localScale.y >= 0.8f && _rectTrans.localScale.z >= 0.8f)
+            {
+
+                _rectTrans.localScale -= new Vector3(0.1f, 0.1f, 0.1f);
+            }
+            
+        }
+        else if(!_isButtonDown)
+        {
+            if (_rectTrans.localScale.x <= 0.9f && _rectTrans.localScale.y <= 0.9f && _rectTrans.localScale.z <= 0.9f)
+            {
+                _rectTrans.localScale += new Vector3(0.1f, 0.1f, 0.1f);
+
+            }
+        }
+         
+    }
+
+}
