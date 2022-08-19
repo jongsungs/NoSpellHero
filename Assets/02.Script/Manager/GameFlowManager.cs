4e279005806eadd56e84f5e77f4183be29e3de74
jong sung kim <cwos17@naver.com>
2022-08-19 17:50:40 +0900

    로딩씬 추가, 버전 변경
    추추가

--- Assets/02.Script/Manager/GameFlowManager.cs
+++ Assets/02.Script/Manager/GameFlowManager.cs
@@ -1,17 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameFlowManager : MonoBehaviour
 {
    static public GameFlowManager Instance { get; private set; }
+	public Fade _fade;
+	
 
 
     private void Awake()
     {
         Instance = this;
+        DontDestroyOnLoad(gameObject);
     }
 
 
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Z))
+        {
+            _fade.FadeOut(2f);
+        }
+
+        if(Input.GetKeyDown(KeyCode.X))
+        {
+            _fade.FadeIn(2f);
+        }
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            LoadSceneManager.LoadScene("GamePlay");
+        }
+
+    }
+
+
+   
 
 }
+
