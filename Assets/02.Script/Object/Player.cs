9efbbd31a329f8f45daf6845632deefdba1bf745
jong sung kim <cwos17@naver.com>
2022-08-25 21:53:59 +0900

    수정

--- Assets/02.Script/Object/Player.cs
+++ Assets/02.Script/Object/Player.cs
@@ -48,6 +48,7 @@ public class Player : BaseObject
 
     private void Awake()
     {
+        Load();
         _data = new PlayerData(_hp, _atk, _matk, _atkSpeed, _def, _speed, _critical, _handicraft, _charm);
         
         _preSpeed = _speed;
@@ -72,21 +73,25 @@ public class Player : BaseObject
 
     public void FixedUpdate()
     {
-        if (variableJoystick._isStop)
+        if (variableJoystick != null)
         {
-            // rb.velocity = Vector3.zero;
-            //rb.angularVelocity = Vector3.zero;
-            _speed = 0f;
-        }
-        else if(variableJoystick._isStop == false)
-        {
-            _speed = _preSpeed;
-        }
-        Vector3 direction = Vector3.forward * variableJoystick.Vertical + Vector3.right * variableJoystick.Horizontal;
-        rb.velocity = (direction * _speed * Time.fixedDeltaTime);
 
-        transform.rotation = Quaternion.LookRotation(direction);
-        transform.Translate(Vector3.forward * _rotateSpeed * Time.deltaTime);
+            if (variableJoystick._isStop)
+            {
+                // rb.velocity = Vector3.zero;
+                //rb.angularVelocity = Vector3.zero;
+                _speed = 0f;
+            }
+            else if (variableJoystick._isStop == false)
+            {
+                _speed = _preSpeed;
+            }
+            Vector3 direction = Vector3.forward * variableJoystick.Vertical + Vector3.right * variableJoystick.Horizontal;
+            rb.velocity = (direction * ((_speed+1) * 50f)* Time.fixedDeltaTime);
+
+            transform.rotation = Quaternion.LookRotation(direction);
+            transform.Translate(Vector3.forward * _rotateSpeed * Time.deltaTime);
+        }
     }
 
     #region File IO
